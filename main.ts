60435f91c4f9747f1d42b5730113001082508253
tech-shrimp <techshrimp@163.com>
2025-01-29 19:26:58 +0800

    首次提交

---
+++ main.ts
@@ -0,0 +1,44 @@
+async function handleRequest(request: Request): Promise<Response> {
+
+  const url = new URL(request.url);
+  const pathname = url.pathname;
+
+  if (pathname === '/' || pathname === '/index.html') {
+    return new Response('Proxy is Running！Details：https://github.com/tech-shrimp/deno-api-proxy', {
+      status: 200,
+      headers: { 'Content-Type': 'text/html' }
+    });
+  } 
+  
+  const targetUrl = `https://${pathname}`;
+
+  try {
+    const headers = new Headers();
+    const allowedHeaders = ['accept', 'content-type', 'authorization'];
+    for (const [key, value] of request.headers.entries()) {
+      if (allowedHeaders.includes(key.toLowerCase())) {
+        headers.set(key, value);
+      }
+    }
+
+    const response = await fetch(targetUrl, {
+      method: request.method,
+      headers: headers,
+      body: request.body
+    });
+
+    const responseHeaders = new Headers(response.headers);
+    responseHeaders.set('Referrer-Policy', 'no-referrer');
+
+    return new Response(response.body, {
+      status: response.status,
+      headers: responseHeaders
+    });
+
+  } catch (error) {
+    console.error('Failed to fetch:', error);
+    return new Response('Internal Server Error', { status: 500 });
+  }
+};
+
+Deno.serve(handleRequest); 
